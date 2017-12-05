3c211e96b054312d3a6424928925d7469c96e8df
elioadriao <elio.adriao@gmail.com>
2017-12-05 12:28:25 -0300

    Evolucao Animal refinada

--- controllers/evoanimalController.js
+++ controllers/evoanimalController.js
@@ -2,31 +2,48 @@
 app.controller("evoanimalController", function($scope, $location, $window){
 
 	$scope.table_name = "evolucao";
-	$scope.primary_key = "id";
-	//List
-	
-	$scope.getTaxas = function(){
+	$scope.primary_key = "propriedadeId_FK";
+		
+	$scope.init = function(){
 		var SQL = "SELECT * FROM "+$scope.table_name+" WHERE propriedadeId_FK="+$scope.getIdPropriedade();
 		
 		basel.database.runAsync(SQL, function(data){
 			if(data[0] != null){
-				return {
-					idadeabate : data[0].idade_abate,
-					natalidade : data[0].natalidade,
-					mortalidadeCria : data[0].mortalidade_cria,
-					mortalidadeRecria : data[0].mortalidade_recria,
-					mortalidadeAdultos : data[0].mortalidade_adultos,
-					descarteMatrizes : data[0].descarte_matrizes,
-					descarteBezerras : data[0].descarte_bezerras,
-					descarteNovilha1 : data[0].descarte_novilha1,
-					descarteNovilha2 : data[0].descarte_novilha2
-				};
+				$scope.itemBanco = data[0];
+				$scope.existe = true;
 			}else{
-				console.log("nao existe");		
+				$scope.existe = false;
+				console.log("nao existe variaveis");
 				$('#evoanimalController').modal('show');
-				return {};
 			}
 		});
+	}
+
+	$scope.getAnimais = function(ano){
+		var SQL = "SELECT * FROM animal WHERE propriedadeId_FK="+$scope.getIdPropriedade()+" AND ano="+ano;
+
+		basel.database.runAsync(SQL, function(data){
+			if(data[0] != null){
+				$scope.itemAnimais = data;
+				console.log("existe animais");
+			}else{
+				console.log("nao existe animais");
+			}
+		});
+	}
+
+	$scope.getTaxas = function(){
+		return {
+				idadeabate : $scope.itemBanco.idade_abate,
+				natalidade : $scope.itemBanco.natalidade/100,
+				mortalidadeCria : $scope.itemBanco.mortalidade_cria/100,
+				mortalidadeRecria : $scope.itemBanco.mortalidade_recria/100,
+				mortalidadeAdultos : $scope.itemBanco.mortalidade_adultos/100,
+				descarteMatrizes : $scope.itemBanco.descarte_matrizes/100,
+				descarteBezerras : $scope.itemBanco.descarte_bezerras/100,
+				descarteNovilha1 : $scope.itemBanco.descarte_novilha1/100,
+				descarteNovilha2 : $scope.itemBanco.descarte_novilha2/100
+		};
 	}
 
 	$scope.updateAno = function(ano){
@@ -81,80 +98,143 @@ app.controller("evoanimalController", function($scope, $location, $window){
 		};
 	}
 
+	// Calcula valor de Compras
 	$scope.getComp = function(ano, tipo){
-		var SQL = "SELECT * FROM animal WHERE propriedadeId_FK="+$scope.getIdPropriedade()+" AND ano="+ano;
-
-		/*basel.database.runAsync(SQL, function(data){
-			if(data[0] != null){
-				return data[tipo-1].qtd;
-				console.log("existe animais");
-			}else{
-				return 0;
-				console.log("nao existe animais");
-			}
-		});*/
-		return 1;
+		$scope.getAnimais(ano);
+	
+		return $scope.itemAnimais[tipo-1].qtd;
 	}
 
+	// Calcula valor de Cabeças Inicial
 	$scope.getCabi = function(ano, tipo){
-		return 1;
+		var res = 0;
+		
+		if(ano == 1){
+			res = $scope.getComp(ano, tipo);
+		}else{
+			res = $scope.getCabf(ano-1, tipo) + $scope.getComp(ano-1, tipo);
+		}
+
+		return res;
 	}
 
+	// Calcula valor de Cabeças Final
 	$scope.getCabf = function(ano, tipo){
-		return 1;
+		var res = 0;
+
+		if(ano == 1){
+			res = $scope.getCabi(ano, tipo) - $scope.getMort(ano, tipo) - $scope.getVend(ano, tipo);
+		}else{
+			res = $scope.getCabi(ano, tipo) + $scope.getComp(ano, tipo) - $scope.getMort(ano, tipo) - $scope.getVend(ano, tipo);
+		}
+
+		return res;
 	}
 
+	// Calcula valor de Nascimentos
 	$scope.getNasc = function(ano, tipo){
-		return 1;
+		var res = 0;
+		
+		if(tipo == 3 || tipo == 6){
+			res = ((($scope.getCabi(ano, 1) * $scope.getTaxas().natalidade) /2) + (($scope.getComp(ano, 1) * $scope.getTaxas().natalidade) /2)) - $scope.getCabi(ano, tipo);
+		}
+
+		return res.toFixed(0);
 	}
 
+	// Calcula valor de Mortes
 	$scope.getMort = function(ano, tipo){
-		return 1;
+		var res = 0;
+
+		switch(tipo){
+			case 3:
+			case 6:
+				res = ($scope.getCabi(ano,tipo) + $scope.getComp(ano,tipo)) * $scope.getTaxas().mortalidadeCria;
+				break;
+			
+			case 4:
+			case 7:
+				res = ($scope.getCabi(ano,tipo) + $scope.getComp(ano,tipo)) * $scope.getTaxas().mortalidadeRecria;
+				break;
+			
+			default:
+				res = ($scope.getCabi(ano,tipo) + $scope.getComp(ano,tipo)) * $scope.getTaxas().mortalidadeAdultos;
+		}
+		
+		return res.toFixed(0);
 	}
 
+	// Calcula valor de Vendas
 	$scope.getVend = function(ano, tipo){
-		return 1;
+		var res = 0;
+
+		switch(tipo){
+			case 1:
+				res = ($scope.getCabi(ano, tipo) - $scope.getMort(ano, tipo)) * $scope.getTaxas().descarteMatrizes;
+				break;
+			
+			case 6:
+				res = ($scope.getCabi(ano, tipo) - $scope.getMort(ano, tipo)) * $scope.getTaxas().descarteBezerras;
+				break;
+			
+			case 7:
+				res = ($scope.getCabi(ano, tipo) - $scope.getMort(ano, tipo)) * $scope.getTaxas().descarteNovilha1;
+				break;
+			
+			case 8:
+				res = ($scope.getCabi(ano, tipo) - $scope.getMort(ano, tipo)) * $scope.getTaxas().descarteNovilha2;
+				break;
+
+			case 9:
+				res = $scope.getCabi(ano, tipo) - $scope.getMort(ano, tipo);
+				break;
+
+			default:
+				res = 0;
+		}
+
+		return res.toFixed(0);
 	}
 
 	$scope.getIdPropriedade = function(){
 		return $window.localStorage.getItem("idPropriedade");
 	}
 
-	//Saving
 	$scope.save = function(){
 		$scope.form.propriedadeId_FK = $scope.getIdPropriedade();
 
-		if($scope.form[$scope.primary_key]){
-			//Edit
+		if($scope.existe){
 			var id = $scope.form[$scope.primary_key];
 			delete $scope.form[$scope.primary_key];
-			delete $scope.form.$$hashKey; //Apaga elemento $$hashKey do objeto
-			basel.database.update($scope.table_name, $scope.form, {PRIMARY_KEY: id}); //entidade, dados, where
+			delete $scope.form.$$hashKey;
+			basel.database.update($scope.table_name, $scope.form, {"propriedadeId_FK": id}); //entidade, dados, where
+			console.log("velho");
 		}else{
-			//new
 			basel.database.insert($scope.table_name, $scope.form); // entidade, dados
+			console.log("novo");
+			$scope.existe = true;
 		}
+		$('#evoanimalController').modal('hide');
 		$scope.form = {};
 		$scope.updateAno(1);
-		$('#evoanimalController').modal('hide');
 	}
 
-	// Cancel form
 	$scope.cancel = function(){
 		$scope.form = {};
 	}
 
 	//Abrindo para editar
-	$scope.edit = function(data){
-		$scope.form = data;
+	$scope.edit = function(){
+		$scope.init();
+		$scope.form = $scope.itemBanco;
 		$('#evoanimalController').modal('show');
 	}
 
-	//Excluindo
+	/*
 	$scope.delete = function(data){
 		if(confirm("Are you sure?")){
 			basel.database.delete($scope.table_name, {PRIMARY_KEY: data[$scope.primary_key]});
 			$scope.list();
 		}
-	}
+	}*/
 });
